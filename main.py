7de70aa591955456cc6b1516a17cb16894d28fc1
saroua <luluorban2@hotmail.com>
2022-02-05 20:13:05 +0100

    1st

---
+++ main.py
@@ -0,0 +1,50 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def user_choice():
+    question = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    return question
+
+
+choice = user_choice()
+# print(user_choice)
+
+while choice not in ["off", "cappuccino", "latte", "report", "espresso"]:
+    print("Invalid input, please enter valid input.")
+    choice = user_choice()
+
+if choice == "off":
+    print("Coffee machine turning off, goodbye.")
+    exit()
+
+print("Correct input")
